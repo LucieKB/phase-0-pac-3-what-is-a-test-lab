b878f03e8f5eff229030d3db41ad314a5e5708b8
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2022-10-30 11:31:38 -0600

    commit tree e467fb08ecf309e2b3e13b857d663076947a2039

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const name = "Joe";
+const name = "Susan";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+
+function multiply(x, y) {
+    return x*y
+}
+console.log(multiply(2,3))
